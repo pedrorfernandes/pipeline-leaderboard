8aa4c37986cee561e6ce610577a3c82f0e80f884
Pedro Fernandes <pedro.fernandes@blip.pt>
2017-01-10 14:51:50 +0000

    rename function

--- crawler/src/job-helper.ts
+++ crawler/src/job-helper.ts
@@ -1,6 +1,6 @@
 import { jenkinsInstance as jenkins } from './jenkins';
 
-function getProductBuild (jobName: string, build: JenkinsBuild, productJobName: string): Promise<JenkinsBuild> {
+function getUpstreamBuild (jobName: string, build: JenkinsBuild, upstreamJobName: string): Promise<JenkinsBuild> {
 
     const { upstreamProject, upstreamBuild } = build.actions
         .filter((action) => action.hasOwnProperty('causes'))
@@ -10,21 +10,21 @@ function getProductBuild (jobName: string, build: JenkinsBuild, productJobName: 
     // TODO lazy load from DB
     if (!upstreamProject) {
         return jenkins.build.get(jobName, build.number - 1)
-            .then((jenkinsBuild) => getProductBuild(jobName, jenkinsBuild, productJobName));
+            .then((jenkinsBuild) => getUpstreamBuild(jobName, jenkinsBuild, upstreamJobName));
     }
 
-    if (upstreamProject === productJobName) {
+    if (upstreamProject === upstreamJobName) {
         return jenkins.build.get(upstreamProject, upstreamBuild);
     }
 
-    if (upstreamProject !== productJobName) {
+    if (upstreamProject !== upstreamJobName) {
         return jenkins.build.get(upstreamProject, upstreamBuild)
-            .then((jenkinsBuild) => getProductBuild(upstreamProject, jenkinsBuild, productJobName));
+            .then((jenkinsBuild) => getUpstreamBuild(upstreamProject, jenkinsBuild, upstreamJobName));
     }
 
     return Promise.reject('Could not find product build');
 }
 
 export {
-    getProductBuild
+    getUpstreamBuild
 }
